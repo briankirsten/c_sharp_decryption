9b4f3a0de0b7cce79cc58a4ba189336757cb1782
Brian Kirsten <brian.kirsten.bss@gmail.com>
2013-11-14 22:25:28 -0500

    Making more generic

--- c_sharp_decryption/Program.cs
+++ c_sharp_decryption/Program.cs
@@ -12,11 +12,11 @@ namespace RSA_Decrypt
 		{
 			string certLocation = "[PRIVATE KEY LOCATION]";
 
-			string encryptedString = "kvxNG/wRBTvTDFsCd3NU3hSzOJV1rqEAzeskl8NSsrWeCk4uA/r2PNFDe9BP70+dc126SWl0/YmDMXb9IFzZgcNxLZ6UnlqZ8MEWkXR7WpFBW0o14nyOMgitcjG2qW5rEZ9OhGVa/+C+h9QUJlHbFe75llZtbw689gDEf17Ms1cuezZN9gmTKruWteN61Jsu8WvgDmt4f+DgElsQYN6MdbNGpaC+wA33e3lGhg3nQCVH+iKd+J73hu8MxM+jmSo0TmMHH87J1HjSwLHuFJQXMtLAq0YKm9mVVCWVCr4jzgb0SgLJ+llESh0fHajb1ah/7ATW7XVRSVBQSpO2v9mWhw==";
+			string encryptedString = "[ENCRYPTED STRING]";
 
 			Encryption encObj = new Encryption (certLocation);
 
-			Console.WriteLine (encObj.Decrypt (encryptedString)); //Should print Hello World!
+			Console.WriteLine (encObj.Decrypt (encryptedString));
 
 		}
 	}
